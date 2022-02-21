5b9172a6ecc66f2780220cf658104e972d1df991
ainasuarez <ainasuarezarenas@gmail.com>
2022-02-21 19:47:03 +0100

    carruselacabat

--- exercicis/src/Carrusel/Carrusel.js
+++ exercicis/src/Carrusel/Carrusel.js
@@ -1,63 +1,39 @@
 import { useState } from "react";
 import "./Carrusel.css";
+const carrusel = [
+  "https://www.ansalatina.com/webimages/al_news_476x/2021/4/19/1798786acbfa9f90c8d92266fddbda42.jpg",
+  "https://offloadmedia.feverup.com/valenciasecreta.com/wp-content/uploads/2019/01/21093537/nutrias-valencia.jpg",
+  "https://t2.ev.ltmcdn.com/es/posts/2/7/0/las_nutrias_son_animales_domesticos_3072_0_600.jpg",
+  "https://cdn.ondavasca.com/nutria.jpg",
+];
 
 function Carrusel() {
-  const carrusel = [
-    "https://www.ansalatina.com/webimages/al_news_476x/2021/4/19/1798786acbfa9f90c8d92266fddbda42.jpg",
-    "https://offloadmedia.feverup.com/valenciasecreta.com/wp-content/uploads/2019/01/21093537/nutrias-valencia.jpg",
-    "https://t2.ev.ltmcdn.com/es/posts/2/7/0/las_nutrias_son_animales_domesticos_3072_0_600.jpg",
-    "https://cdn.ondavasca.com/nutria.jpg",
-  ];
-
   const [imatgeclicada, setImatgeclicada] = useState(0);
 
   const passar = (canvi) => {
-    if (imatgeclicada === 3) {
-      setImatgeclicada(0);
-    }
-    if (imatgeclicada === 0) {
-      setImatgeclicada(3);
-    } else {
-      setImatgeclicada(imatgeclicada + canvi);
-    }
+    let n = imatgeclicada + canvi;
+    let l = carrusel.length;
+    n = (n + l) % l;
+    setImatgeclicada(n);
   };
 
   return (
     <>
-      <h1>Carrusel</h1>
+      <h1>Carrusel {imatgeclicada}</h1>
       <div className="caixa">
         <button onClick={() => passar(0 - 1)}> ← </button>
 
-        <img
-          className={imatgeclicada === 0 ? "mostra" : "amaga"}
-          src={carrusel[0]}
-          alt="imatge 1"
-        />
-        <img
-          className={imatgeclicada === 1 ? "mostra" : "amaga"}
-          src={carrusel[1]}
-          alt="imatge 2"
-        />
-        <img
-          className={imatgeclicada === 2 ? "mostra" : "amaga"}
-          src={carrusel[2]}
-          alt="imatge 3"
-        />
-        <img
-          className={imatgeclicada === 3 ? "mostra" : "amaga"}
-          src={carrusel[3]}
-          alt="imatge 4"
-        />
+        <img alt="carrussel" src={carrusel[imatgeclicada]}></img>
+        <br />
+
         <button onClick={() => passar(0 + 1)}> → </button>
       </div>
-      <div>
-        <button type="circle" onClick={() => carrusel[0]}>
-          ●
+
+      {carrusel.map((url, index) => (
+        <button key={url} onClick={() => setImatgeclicada(index)}>
+          {imatgeclicada === index ? "○" : "●"}
         </button>
-        <button type="circle">●</button>
-        <button type="circle">○</button>
-        <button type="circle">●</button>
-      </div>
+      ))}
     </>
   );
 }
